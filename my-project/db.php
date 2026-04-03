85b2c01ba204692f596c680d5d9d3f1a0eaa387d
Hayrunnisa Yoltan <hayrunnisayoltan@gmail.com>
2026-04-03 19:39:29 +0300

    add

--- my-project/db.php
+++ my-project/db.php
@@ -1,13 +1,9 @@
 <?php
 require 'vendor/autoload.php';
 
-// BURASI ÇOK KRİTİK: 'localhost' yazısını tamamen silmelisin!
-// Atlas'tan aldığın gerçek bağlantı linkini buraya yapıştır.
-$uri = "mongodb+srv://dbHayrunnisa:1377Nisa.@cluster0.ibqgzo4.mongodb.net/?appName=Cluster0;
-
+$uri = "mongodb+srv://dbHayrunnisa:1377Nisa.@cluster0.ibqgzo4.mongodb.net/?appName=Cluster0";
 
 $client = new MongoDB\Client($uri);
-    // Veritabanı adını Atlas'takiyle aynı yap
-$db = $client->selectDatabase('veteriner_db'); 
 
+$db = $client->selectDatabase('veteriner_db');
 ?>
